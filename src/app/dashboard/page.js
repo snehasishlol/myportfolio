b8ee8a4b3752d2fcf5aca701aaf0b4ac911fb043
SiSH0001 <snehasishray.acs9c@gmail.com>
2023-03-11 19:38:11 +0530

    Bug fix

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,7 +7,7 @@ export default function DashboardPage() {
     <>
         <Dashboard
             items={[
-                { name: 'Home', icon: FiHome, href: '/' }
+                { name: 'Home', icon: <FiHome/>, href: '/' }
             ]}>
             This is your dashboard. On the left you can see the Dashboard Sidebar Items. On the top right are some default buttons and a User menu.
         </Dashboard>
