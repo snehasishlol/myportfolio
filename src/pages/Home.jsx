cef2fa1963a2f2873a57c15671c8ea95b8160f73
snehasishkun <snehasishcodes@gmail.com>
2023-06-29 15:18:25 +0530

    okayy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,8 @@ import {
     Text,
     Tag,
     TagLabel,
-    HStack
+    HStack,
+    SimpleGrid
 } from '@chakra-ui/react';
 import { Kanit } from 'next/font/google';
 
@@ -27,140 +28,71 @@ export default function Home() {
             minHeight={'100vh'}
         >
             <Box
-                minWidth={'lg'}
+                w={{ base: 'xs', sm: 'sm', md: '3xl' }}
                 backdropBlur={'10px'}
                 boxShadow={'lg'}
                 bg={'black'}
                 borderRadius={'lg'}
                 borderColor={'rgba(255, 255, 255, 0.3)'}
                 backdropFilter={'blur(6px)'}
-                p={8}
+                p={4}
+                maxH={'80vh'}
+                overflow={'scroll'}
+                css={{ '&::-webkit-scrollbar': { width: '0' } }}
             >
-                <Tabs colorScheme={'green'}>
+                <Image
+                    h={'100px'}
+                    w={'full'}
+                    src={
+                        '/media/banners/29.06.2023.png'
+                    }
+                    objectFit={'cover'}
+                    borderRadius={'sm'}
+                />
+                <Flex justify={'center'} mt={-12}>
+                    <Avatar
+                        size={'xl'}
+                        src={
+                            '/media/avatars/29.06.2023.jpg'
+                        }
+                        alt={'snehasish'}
+                        css={{
+                            border: '5px solid black'
+                        }}
+                    />
+                </Flex>
+                <Box px={8} py={2}>
+                    <Text fontSize={'xl'} color={'green.300'} textAlign={'center'}>snehasishTwT</Text>
+                    <Text fontSize={'sm'} color={'gray.300'} textAlign={'center'}>@snehasish</Text>
+                    <Text textAlign={'center'} my={2}>hey! i am <Text as={'span'} color={'green.300'}>snehasish</Text>. and i make some <Text as={'span'} color={'blue.300'}>awesome</Text> <Text as={'span'} color={'teal.300'}>stuff</Text> with <Text as={'span'} color={'red.300'}>code</Text>.</Text>
+                    <br />
+                    <Text className={kanit.className} textTransform={'uppercase'} fontWeight={'bold'} color={'green.300'}>ROLES</Text>
+                    <Flex direction={'row'} flexWrap={'wrap'} gap={1}>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>He/Him</Tag>
+                        <Tag size={'md'} colorScheme='green' borderRadius='full'>16</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>India</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>Programming</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>Books</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>Physics</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>Space</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>Anime</Tag>
+                        <Tag size={'md'} colorScheme='blue' borderRadius='full'>Music</Tag>
+                    </Flex>
+                </Box>
+
+                <Tabs colorScheme={'green'} isFitted>
                     <TabPanels>
-                        <TabPanel index={'about'}>
-                            <Image
-                                h={'120px'}
-                                w={'full'}
-                                src={
-                                    '/media/banners/29.06.2023.png'
-                                }
-                                objectFit={'cover'}
-                                borderRadius={'sm'}
-                            />
-                            <Flex justify={'center'} mt={-12}>
-                                <Avatar
-                                    size={'xl'}
-                                    src={
-                                        '/media/avatars/29.06.2023.jpg'
-                                    }
-                                    alt={'snehasish'}
-                                    css={{
-                                        border: '5px solid black'
-                                    }}
-                                />
-                            </Flex>
-                            <Text fontSize={'xl'} color={'green.300'}>snehasishTwT</Text>
-                            <Text fontSize={'sm'}>@snehasish</Text>
-                            <br />
-                            <Text className={kanit.className} textTransform={'uppercase'} fontWeight={'bold'} color={'green.300'}>ABOUT ME</Text>
-                            <Text>hey! i am <Text as={'span'} color={'green.300'}>snehasish</Text>. and i make some <Text as={'span'} color={'blue.300'}>awesome</Text> <Text as={'span'} color={'teal.300'}>stuff</Text> with <Text as={'span'} color={'red.300'}>code</Text>.</Text>
-                            <br />
-                            <Text className={kanit.className} textTransform={'uppercase'} fontWeight={'bold'} color={'green.300'}>ROLES</Text>
-                            <HStack spacing={2}>
-                                <Tag size='md' colorScheme='blue' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/boy.png'
-                                        size='xs'
-                                        name='boy'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>He/Him</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='green' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/anime.png'
-                                        size='xs'
-                                        name='anime'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>16</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='orange' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/india.png'
-                                        size='xs'
-                                        name='india'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>India</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='blue' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/code.png'
-                                        size='xs'
-                                        name='code'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>Coding</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='green' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/book.png'
-                                        size='xs'
-                                        name='book'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>Books</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='red' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/physics.png'
-                                        size='xs'
-                                        name='physics'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>Physics</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='yellow' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/planet.png'
-                                        size='xs'
-                                        name='planet'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>Astronomy</TagLabel>
-                                </Tag>
-                                <Tag size='md' colorScheme='green' borderRadius='full'>
-                                    <Avatar
-                                        src='/media/roles/anime.png'
-                                        size='xs'
-                                        name='anime'
-                                        ml={-1}
-                                        mr={2}
-                                    />
-                                    <TagLabel>Anime</TagLabel>
-                                </Tag>
-                            </HStack>
+                        <TabPanel index={'info'}>
+
                         </TabPanel>
                         <TabPanel>
 
                         </TabPanel>
                     </TabPanels>
                     <TabList>
-                        <Tab>About</Tab>
-                        <Tab>Knowledge</Tab>
-                        <Tab>Projects</Tab>
-                        <Tab>Connections</Tab>
-                        <Tab>Activity</Tab>
-                        <Tab>Message</Tab>
+                        <Tab>INFO</Tab>
+                        <Tab>PROJECTS</Tab>
+                        <Tab>SOCIALS</Tab>
                     </TabList>
                 </Tabs>
             </Box>
